8eee0bc641ca1346dc91c85fe4736f48a358269a
Jan Halfar <jan@bestbytes.com>
2019-01-13 11:18:25 +0100

    chore: index cleanup

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,26 +1,21 @@
 import React from "react";
 import Link from "next/link";
-import styled from "styled-components";
 import { Page, Title, List, ListItem } from "../components/components";
 
-const Index = (props) => {
+const Index = () => {
   return (
     <Page>
       <Title>Index</Title>
       <List>
         <ListItem>
           <Link href="/status">
-            <a>Mein Status</a>
+            <a>Status</a>
           </Link>
         </ListItem>
         <ListItem>
-          <ul>
-            <ListItem>
-              <Link href="/practice">
-                <a>Practice WS 1</a>
-              </Link>
-            </ListItem>
-          </ul>
+          <Link href="/practice">
+            <a>Practice</a>
+          </Link>
         </ListItem>
         <ListItem>
           <Link href="/words">
@@ -29,16 +24,11 @@ const Index = (props) => {
         </ListItem>
         <ListItem>
           <Link href="/edit">
-            <a>Edit</a>
+            <a>Add</a>
           </Link>
         </ListItem>
       </List>
     </Page>
   );
 };
-/*
-      <SolutionList>
-        <SolutionItem>loben</SolutionItem>
-      </SolutionList>
-*/
 export default Index;
